0bf3a1c648fa238d615e86d2a036a6a90d29de99
Richi Thampan <richithampan@gmail.com>
2018-03-06 15:14:07 -0500

    worked on form

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,13 @@ const tweetBank = require('../tweetBank');
 
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
-  res.render( 'index', { tweets: tweets } );
+  res.render( 'index', { tweets: tweets, showForm: true} );
 });
 
 router.get('/users/:name', function(req, res) {
   var name = req.params.name;
   var list = tweetBank.find( {name: name} );
-  res.render( 'index', { tweets: list } );
+  res.render( 'index', { tweets: list, showForm: true } );
 });
 
 router.get('/tweets/:id', function(req, res) {
@@ -20,5 +20,12 @@ router.get('/tweets/:id', function(req, res) {
   var tweetWeFound = tweetBank.find( {id: tweetId })
   res.render( 'index', { tweets: tweetWeFound });
 })
+
+router.post('/tweets', function(req, res) {
+  var name = req.body.name;
+  var text = req.body.text;
+  tweetBank.add(name, text);
+  res.redirect('/');
+});
 
 module.exports = router;
